d7302d1451c1cc1ce69a3f6f0135d19286c0c97f
admin <admin@DESKTOP-MMID23J>
2022-08-23 10:18:29 +0900

    Add project files.

---
+++ Csharp/01_HelloWorld/01_first_project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_first_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* vẽ hình trái tim bằng các ký tự 0 , đơn giản chỉ là in dòng chữ ra màn hình */
+            Console.Title = "My first project in Csharp";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("*****************************");
+            Console.WriteLine();
+            Console.WriteLine("***     0 0 0   0 0 0     ***");
+            Console.WriteLine("***   0 0 0 0 0 0 0 0 0   ***");
+            Console.WriteLine("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
+            Console.WriteLine("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
+            Console.WriteLine("***   0 0 0 0 0 0 0 0 0   ***");
+            Console.WriteLine("***     0 0 0 0 0 0 0     ***");
+            Console.WriteLine("***       0 0 0 0 0       ***");
+            Console.WriteLine("***         0 0 0         ***");
+            Console.WriteLine("***           0           ***");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Csharp/01_HelloWorld/UserInput/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// full name, age, avg mark
+            Console.WriteLine("Ho va ten: ");
+            string fullName = Console.ReadLine();
+
+            Console.WriteLine("Tuoi: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            int age = System.Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Diem TB: ");
+            //float avgMark = Convert.ToSingle(Console.ReadLine());
+            float avgMark = System.Single.Parse(Console.ReadLine());
+
+            Console.WriteLine("======================================");
+            Console.WriteLine("======================================");
+            Console.WriteLine("======================================");
+            Console.WriteLine();
+            Console.WriteLine("Hello, My full name is " + fullName);
+            Console.WriteLine("I'm " + age);
+            Console.WriteLine("My avg mark is " + avgMark);
+        }
+    }
+}
