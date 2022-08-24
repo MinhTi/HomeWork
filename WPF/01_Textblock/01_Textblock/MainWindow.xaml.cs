93a139b2355cd93a4e88e14d281f1db6a2f67ba2
MinhTien <minhtien291296@gmail.com>
2022-08-25 05:45:19 +0900

    bài 2 + 3 về textbox

---
+++ WPF/01_Textblock/01_Textblock/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace _01_Textblock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TextBlock txtbl = sender as TextBlock;
+            txtbl.Text = "Free Education";
+        }
+        private void TextBlock_MouseEnter_link(object sender, MouseEventArgs e)
+        {
+            TextBlock txtbl = sender as TextBlock;
+            txtbl.Text = "www.HowKteam.com";
+        }
+
+        private void TextBlock_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
